8174fbcd2ea6c7804f0785e53f07236e7e01543b
Mathias Rüdiger <ruediger@blueboot.org>
2014-12-28 15:48:10 +0100

    switched font to Pragmata

--- config/theme.el
+++ config/theme.el
@@ -2,5 +2,4 @@
 (load-theme 'solarized-light t)
 (setq solarized-high-contrast-mode-line t)
 (setq solarized-distinct-fringe-background t)
-
-(add-to-list 'default-frame-alist '(font . "Droid Sans Mono-9"))
+(add-to-list 'default-frame-alist '(font . "PragmataPro-9"))
