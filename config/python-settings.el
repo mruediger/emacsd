ad3cdf2078562ffac1bc9b1034bf61db2cc43f20
Mathias Rüdiger <ruediger@blueboot.org>
2015-10-28 14:42:47 +0100

    added <f7> binding to run current buffer with python

--- config/python-settings.el
+++ config/python-settings.el
@@ -3,3 +3,7 @@
 (add-hook 'python-mode-hook
           (lambda() (local-set-key (kbd "<f6>")
                                    (lambda() (interactive) (shell-command (concat "nosetests -s " (buffer-file-name)))))))
+
+(add-hook 'python-mode-hook
+          (lambda() (local-set-key (kbd "<f7>")
+                                   (lambda() (interactive) (shell-command (concat "python " (buffer-file-name)))))))
