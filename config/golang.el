8d1a4aec3cde296b93f2d0eeed07068ee760e6ac
Mathias Rüdiger <ruediger@blueboot.org>
2015-10-28 14:43:36 +0100

    small lisp code refactoring

--- config/golang.el
+++ config/golang.el
@@ -4,9 +4,12 @@
 ;  '(define-key global-map (kbd "<f6>")
 ;     (lambda() (interactive) (shell-command "go test"))))
 
-(add-hook 'go-mode-hook
-          (lambda() (local-set-key (kbd "<f6>")
-                                   (lambda() (interactive) (shell-command "go test")))))
+(defun gotest ()
+  (interactive)
+  (shell-command "go test"))
 
-(add-hook 'go-mode-hook (lambda()
-                          (local-set-key (kbd "C-c C-f") 'gofmt)))
+(add-hook 'go-mode-hook
+          (lambda() (local-set-key (kbd "<f6>") 'gotest)))
+
+(add-hook 'go-mode-hook
+          (lambda() (local-set-key (kbd "C-c C-f") 'gofmt)))
