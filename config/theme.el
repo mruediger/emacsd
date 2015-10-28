069701c234c713c7904fa4ade1f529ec222fe54f
Mathias Rüdiger <ruediger@blueboot.org>
2015-10-28 14:44:33 +0100

    changed theme to darktooth

--- config/theme.el
+++ config/theme.el
@@ -1,5 +1,5 @@
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
-(load-theme 'solarized-light t)
+(load-theme 'darktooth t)
 (setq solarized-high-contrast-mode-line t)
 (setq solarized-distinct-fringe-background t)
-(add-to-list 'default-frame-alist '(font . "PragmataPro-9"))
+(add-to-list 'default-frame-alist '(font . "PragmataPro-10"))
