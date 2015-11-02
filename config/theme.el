51bd4f5fe5c8807fbeb24e622b62b08ae991d4d6
Mathias Rüdiger <ruediger@blueboot.org>
2015-11-02 11:37:19 +0100

    updated darktooth-theme.el

--- config/theme.el
+++ config/theme.el
@@ -1,5 +1,5 @@
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
 (load-theme 'darktooth t)
-(setq solarized-high-contrast-mode-line t)
-(setq solarized-distinct-fringe-background t)
+;;(setq solarized-high-contrast-mode-line t)
+;;(setq solarized-distinct-fringe-background t)
 (add-to-list 'default-frame-alist '(font . "PragmataPro-10"))
