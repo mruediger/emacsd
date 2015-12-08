d1e48413e68ea081ca8ef4a1ee8a0b0b121ddeec
Mathias Rüdiger <ruediger@blueboot.org>
2015-12-08 13:39:23 +0100

    added goimports setting

--- config/golang.el
+++ config/golang.el
@@ -1,8 +1,6 @@
 (require 'go-mode-load)
 
-;(eval-after-load "go-mode"
-;  '(define-key global-map (kbd "<f6>")
-;     (lambda() (interactive) (shell-command "go test"))))
+(setq gofmt-command "goimports")
 
 (defun gotest ()
   (interactive)
