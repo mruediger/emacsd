c59f0bc1d719b0d3bd421b8e46b13876822b598e
Mathias Rüdiger <ruediger@blueboot.org>
2017-08-06 02:37:53 +0200

    add racer to rust.el and refactor everything into use-package

--- config/rust.el
+++ config/rust.el
@@ -1,2 +1,28 @@
-(autoload 'rust-mode "rust-mode" nil t)
-(add-to-list 'auto-mode-alist (cons "\\.rs\\'" 'rust-mode))
+(defconst rust-src-path
+  "/home/bag/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/src/")
+
+(use-package racer
+  :ensure t
+  :init
+  (add-hook 'racer-mode-hook #'eldoc-mode)
+  (setq racer-rust-src-path rust-src-path))
+
+(defun cargo-test ()
+  (interactive)
+  (compile "cargo test"))
+
+(defun cargo-run ()
+  (interactive)
+  (compile "cargo run"))
+
+(use-package rust-mode
+  :ensure t
+  :mode "\\.rs\\'"
+  :init
+  (add-hook 'rust-mode-hook #'racer-mode)
+  :bind (("<f6>" . cargo-test)
+         ("<f7>" . cargo-run)))
+  
+
+;; todo
+;; https://github.com/flycheck/flycheck-rust
--- custom.el
+++ custom.el
@@ -3,7 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (powerline use-package))))
+ '(package-selected-packages
+   (quote
+    (racer racer-mode rust-mode yaml-mode use-package projectile powerline multiple-cursors markdown-mode mark-multiple magit lua-mode hcl-mode golint go-mode flx-ido expand-region ensime dockerfile-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
