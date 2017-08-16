39d00c4ab09ca9077188a630ad330db2e95bb7ca
Mathias Rüdiger <ruediger@blueboot.org>
2017-08-16 14:34:28 +0200

    updated python settings

--- config/python-settings.el
+++ config/python-settings.el
@@ -1,9 +1,14 @@
 (require 'python)
 
-(add-hook 'python-mode-hook
-          (lambda() (local-set-key (kbd "<f6>")
-                                   (lambda() (interactive) (shell-command (concat "nosetests -s " (buffer-file-name)))))))
+(defun python-test ()
+  (interactive)
+  (compile "nosetests -s"))
+
+(defun python-run ()
+  (interactive)
+  (compile (concat "python " buffer-file-name)))
 
 (add-hook 'python-mode-hook
-          (lambda() (local-set-key (kbd "<f7>")
-                                   (lambda() (interactive) (shell-command (concat "python " (buffer-file-name)))))))
+          (lambda() (local-set-key (kbd "<f6>") 'python-test)))
+(add-hook 'python-mode-hook
+          (lambda() (local-set-key (kbd "<f7>") 'python-run)))
