556f015d7104ccc84e0dba6900bb96da0c6e3fcf
Mathias Rüdiger <ruediger@blueboot.org>
2019-11-05 17:38:15 +0100

    wip

--- init.el
+++ init.el
@@ -60,8 +60,8 @@
 ;;
 
 ;; MOUSE
-(when window-system
-  (setq mouse-autoselect-window t))
+;;(when window-system
+(setq mouse-autoselect-window t)
 
 (setq mouse-yank-at-point t)
 
@@ -149,9 +149,18 @@
 
 ;;ORG
 (use-package org
-  :bind*
-  (("C-c o c" . org-capture)
-   ("C-c o a" . org-agenda)))
+  :config
+  (org-babel-do-load-languages 'org-babel-load-languages
+							   '((shell      . t)
+								 (emacs-lisp . t)
+								 (python     . t)))
+  :init
+  (setq org-src-fontify-natively t   ;; Pretty code blocks
+        org-src-tab-acts-natively t
+		org-confirm-babel-evaluate nil
+        org-return-follows-link t))
+
+
 
 ;;MAGIT
 (setq vc-handled-backends nil)
@@ -243,6 +252,48 @@
 
 ;;NixOS
 (use-package nix-mode)
+
+;;Karteikarten
+(use-package pamparam)
+
+
+;;
+;; EMail setup for emacs
+;; based on https://medium.com/@enzuru/emacs-26-wanderlust-and-modern-gmail-authentication-36e1ae61471f
+;;
+(use-package wanderlust
+  :init
+  (autoload 'wl "wl" "Wanderlust" t)
+  :config
+  (setq elmo-imap4-default-server "imap.gmail.com"
+        elmo-imap4-default-user "djpade@gmail.com"
+        elmo-imap4-default-authenticate-type 'clear
+        elmo-imap4-default-port '993
+        elmo-imap4-default-stream-type 'ssl
+        elmo-imap4-use-modified-utf7 t)
+  (setq wl-smtp-connection-type 'starttls
+        wl-smtp-posting-port 587
+        wl-smtp-authenticate-type "plain"
+        ;;wl-smtp-authenticate-type "login"
+        wl-smtp-posting-user "djpade"
+        wl-smtp-posting-server "smtp.gmail.com"
+        wl-local-domain "gmail.com"
+        wl-message-id-domain "smtp.gmail.com")
+  (setq wl-from "Daniel Pade <djpade@gmail.com>"
+
+        ;;all system folders (draft, trash, spam, etc) are placed in the
+        ;;[Gmail]-folder, except inbox. "%" means it's an IMAP-folder
+        wl-default-folder "%inbox"
+        wl-draft-folder   "%[Gmail]/Drafts"
+        wl-trash-folder   "%[Gmail]/Trash"
+        wl-fcc            "%[Gmail]/Sent"
+
+        ;; mark sent messages as read (sent messages get sent back to you and
+        ;; placed in the folder specified by wl-fcc)
+        wl-fcc-force-as-read    t
+
+        ;;for when auto-compleating foldernames
+        wl-default-spec "%"))
 
 ;;
 ;; HELPER FUNCTIONS ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
