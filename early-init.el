2d519826b960e242de6a72a5839b112710259d06
Mathias Rüdiger <ruediger@blueboot.org>
2022-06-01 12:24:44 +0200

    add early-init.el from doom emacs

---
+++ early-init.el
@@ -0,0 +1,14 @@
+;;; early-init.el -*- lexical-binding: t; -*-
+
+;; Emacs 27.1 introduced early-init.el, which is run before init.el, before
+;; package and UI initialization happens, and before site files are loaded.
+
+;; A big contributor to startup times is garbage collection. We up the gc
+;; threshold to temporarily prevent it from running, then reset it later by
+;; enabling `gcmh-mode'. Not resetting it will cause stuttering/freezes.
+(setq gc-cons-threshold most-positive-fixnum)
+
+;; Prevent unwanted runtime compilation for gccemacs (native-comp) users;
+;; packages are compiled ahead-of-time when they are installed and site files
+;; are compiled when gccemacs is installed.
+(setq native-comp-deferred-compilation nil)
