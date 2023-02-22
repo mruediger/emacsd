7b83577c14b28f18dcadf16aa9f45710fbc46eda
Mathias Rüdiger <ruediger@blueboot.org>
2023-02-22 22:15:16 +0100

    switch package management back to use-package :ensure

--- init.el
+++ init.el
@@ -1,6 +1,16 @@
 ;;
 ;; PACKAGE MANAGEMENT
 ;;
+(require 'package)
+(add-to-list 'package-archives '("gnu"   . "https://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(setq use-package-always-ensure t)
 
 (require 'use-package)
 
