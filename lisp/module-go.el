5aec3a7905f8f99fe2eed541023fe99ba5c46dd7
Mathias Rüdiger <ruediger@blueboot.org>
2023-03-20 08:20:00 +0100

    update go config

--- lisp/module-go.el
+++ lisp/module-go.el
@@ -1,27 +1,31 @@
 (require 'module-lsp)
 
-(add-to-list 'auto-mode-alist '("\\.go\\'" . go-mode))
+;; switch to go-mode if go-ts-mode gets loaded
+(add-hook 'go-ts-mode-hook 'go-mode)
 
-(with-eval-after-load 'go-ts-mode
-  (keymap-set go-ts-mode-map "C-c C-c" 'go-test-current-project)
+(with-eval-after-load 'go-mode
+  (keymap-set go-mode-map "C-c C-c" 'go-test-current-project)
 
   ;; CamelCase aware editing operations
   (subword-mode +1)
 
-  (setq  go-ts-mode-indent-offset 4)
+  (setq go-test-verbose t)
+
 
   (defun lsp-go-install-save-hooks ()
     (add-hook 'before-save-hook #'lsp-format-buffer t t)
     (add-hook 'before-save-hook #'lsp-organize-imports t t))
 
-  (add-hook 'go-ts-mode-hook
+  (add-hook 'go-mode-hook
             (lambda ()
               (setq tab-width 2)
               (setq standard-indent 2)
               (setq indent-tabs-mode nil)))
 
-  (add-hook 'go-ts-mode-hook 'lsp-go-install-save-hooks)
-  (add-hook 'go-ts-mode-hook 'flycheck-mode)
-  (add-hook 'go-ts-mode-hook 'lsp-deferred))
+
+
+  (add-hook 'go-mode-hook 'lsp-go-install-save-hooks)
+  (add-hook 'go-mode-hook 'flycheck-mode)
+  (add-hook 'go-mode-hook 'lsp-deferred))
 
 (provide 'module-go)
