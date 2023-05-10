15dcf83790a09aa57b453a72f15884b4dc47c93b
Mathias Rüdiger <ruediger@blueboot.org>
2023-05-10 14:12:27 +0200

    move config back to use-package and straight

--- init.el
+++ init.el
@@ -9,11 +9,24 @@
 (require 'module-magit)
 (require 'module-nix)
 (require 'module-terraform)
-(require 'module-org)
 
 ;;
 ;; PACKAGE MANAGEMENT
 ;;
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(straight-use-package 'use-package)
 (setq use-package-always-defer t)
 (require 'use-package)
 
@@ -21,6 +34,59 @@
 (use-package direnv
   :init
   (direnv-mode))
+
+
+;;
+;; Org Mode
+;;
+(use-package org :straight t
+  :config
+  (use-package org-trello :straight (:build (:not compile)))
+
+  (add-to-list 'org-modules 'org-tempo t)
+  (org-babel-do-load-languages 'org-babel-load-languages
+			       '((shell      . t)
+			         (emacs-lisp . t)
+			         (python     . t)
+                                 (R          . t)
+                                 (gnuplot    . t)
+                                 (js         . t)))
+
+  (setq org-confirm-babel-evaluate nil)
+  ;; Pretty code blocks
+  (setq org-src-fontify-natively t
+        org-src-tab-acts-natively t)
+
+  (setq org-duration-format (quote h:mm))
+
+  (setq org-directory "~/org"
+        org-refile-use-outline-path 'file)
+  (setq org-agenda-files '("~/org")
+        org-agenda-window-setup (quote current-window))
+  (setq org-capture-templates
+        '(("t" "task" entry (file "inbox.org") "* TODO %?\n")
+          ("n" "note" entry (file "inbox.org") "* TODO %?\n %a")))
+  (setq org-export-with-toc nil)
+  (setq org-agenda-custom-commands
+        '(("c" "Agenda and TODO" ((agenda "" ((org-agenda-span 9)
+                                              (org-agenda-start-day "-2d")))
+                                  (alltodo "" ((org-agenda-todo-ignore-deadlines (quote all))
+                                               (org-agenda-todo-ignore-scheduled (quote all))))))))
+
+  (defun org-agenda-show-agenda-and-todo () (interactive) (org-agenda nil "c"))
+  (defun org-open-inbox () (interactive) (find-file "~/org/inbox.org"))
+  (defun org-open-today () (interactive) (find-file (concat "~/org/today-" (format-time-string "%Y-%m-%d") ".org")))
+  :bind
+  ("C-x o a" . org-agenda-show-agenda-and-todo)
+  ("C-x o t" . org-todo-list)
+  ("C-x o c" . org-capture)
+  ("C-x o i" . org-open-inbox)
+  ("C-x o n" . org-open-today)
+  :hook
+  (org-mode . visual-line-mode))
+
+
+
 
 ;;
 ;; Development
