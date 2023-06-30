fd087b4ee97b3370a1eb4dc7304254b9514d97bb
Mathias Rüdiger <ruediger@blueboot.org>
2023-06-30 17:36:15 +0200

    update theme settings

--- init.el
+++ init.el
@@ -68,14 +68,15 @@
   (setq which-key-idle-delay 0.5))
 
 (use-package solarized-theme :straight t
-  :init (load-theme 'solarized-light t)
-  :config
+  :init
   (setq solarized-scale-org-headlines nil
         solarized-use-variable-pitch nil
         solarized-height-plus-1 1.0
         solarized-height-plus-2 1.0
         solarized-height-plus-3 1.0
         solarized-height-plus-4 1.0)
+
+  (load-theme 'solarized-light t)
 
   (let ((line (face-attribute 'mode-line :underline)))
     (set-face-attribute 'mode-line          nil :overline   line)
