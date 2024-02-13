f9ad6f415af554ab306f6dc6dc0f8d0af3e09443
Mathias Rüdiger <ruediger@blueboot.org>
2024-02-13 13:09:26 +0100

    add init-ui.el

--- init.el
+++ init.el
@@ -5,6 +5,8 @@
 
 (require 'init-bootstrap)
 (require 'init-gc)
+(require 'init-ui)
+
 (require 'init-editor)
 (require 'init-keybindings)
 (require 'init-email)
@@ -12,49 +14,13 @@
 ;;
 ;; Setup UI
 ;;
-(setq inhibit-startup-screen t
-      initial-scratch-message nil)
 
-;; cleanup user interface - remove unneded fluff
-(scroll-bar-mode -1)   ; Disable visible scrollbar
-(tool-bar-mode -1)     ; Disable toolbar
-(tooltip-mode -1)      ; Disable tooltips
-(menu-bar-mode -1)     ; Disable menu bar
-(blink-cursor-mode -1)
-
-;; mode line settings
-(line-number-mode t)
-(column-number-mode t)
-(size-indication-mode t)
-(display-time-mode t)
-
-(set-fontset-font "fontset-default" nil
-                  (font-spec :size 20 :name "FontAwesome"))
-
-(set-face-attribute 'default nil
-                    :font "Iosevka"
-                    :height 120)
-
-(setq frame-title-format "emacs: %b")
-
-;;split windows proportionally
-(setq window-combination-resize 't)
 
 ;; MOUSE
 (setq mouse-autoselect-window nil)
 (setq mouse-yank-at-point t)
 (setq make-pointer-invisible nil)
 
-(use-package gruvbox-theme :straight t
-  :config
-  (load-theme 'gruvbox-light-medium t))
-
-(use-package moody :straight t
-  :init
-  (setq x-underline-at-descent-line t)
-  (moody-replace-mode-line-buffer-identification)
-  (moody-replace-vc-mode)
-  (moody-replace-eldoc-minibuffer-message-function))
 
 ;; direnv
 (use-package direnv :straight t
---
+++ lisp/init-ui.el
@@ -0,0 +1,41 @@
+(setq inhibit-startup-screen t
+      initial-scratch-message nil)
+
+;; cleanup user interface - remove unneded fluff
+(scroll-bar-mode -1)   ; Disable visible scrollbar
+(tool-bar-mode -1)     ; Disable toolbar
+(tooltip-mode -1)      ; Disable tooltips
+(menu-bar-mode -1)     ; Disable menu bar
+(blink-cursor-mode -1)
+
+;; mode line settings
+(line-number-mode t)
+(column-number-mode t)
+(size-indication-mode t)
+(display-time-mode t)
+
+(set-fontset-font "fontset-default" nil
+                  (font-spec :size 20 :name "FontAwesome"))
+
+(set-face-attribute 'default nil
+                    :font "Iosevka"
+                    :height 120)
+
+
+(setq frame-title-format "emacs: %b")
+
+;;split windows proportionally
+(setq window-combination-resize 't)
+
+(use-package gruvbox-theme :straight t
+  :config
+  (load-theme 'gruvbox-light-medium t))
+
+(use-package moody :straight t
+  :init
+  (setq x-underline-at-descent-line t)
+  (moody-replace-mode-line-buffer-identification)
+  (moody-replace-vc-mode)
+  (moody-replace-eldoc-minibuffer-message-function))
+
+(provide 'init-ui)
