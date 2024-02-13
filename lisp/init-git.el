28290041e95020015590c52c59b4c354d0bee6c1
Mathias Rüdiger <ruediger@blueboot.org>
2024-02-13 13:10:10 +0100

    add init-git

---
+++ lisp/init-git.el
@@ -0,0 +1,28 @@
+(use-package magit :straight t
+  :bind
+  (("C-x g s"   . magit-status)
+   ("C-x g p"   . magit-push-current-to-pushremote)
+   ("C-x g l b" . magit-log-buffer-file)
+   ("C-x g l c" . magit-log-current)
+   ("C-x g l a" . magit-log-all)))
+
+(use-package forge :straight t
+  :after magit)
+
+;;(use-package code-review
+;;  :straight (:host github :repo "phelrine/code-review" :branch "fix/closql-update")
+;;  :after magit
+;;  :custom
+;;  (code-review-download-dir (concat minemacs-cache-dir "code-review/"))
+;;  (code-review-db-database-file (concat minemacs-local-dir "code-review/database.sqlite"))
+;;  (code-review-log-file (concat minemacs-local-dir "code-review/code-review-error.log"))
+;;  (code-review-auth-login-marker 'forge) ; use the same credentials as forge in ~/.authinfo.gpg
+;;  :init
+;;  (with-eval-after-load 'magit
+;;    (transient-append-suffix 'magit-merge "i"
+;;      '("y" "Review pull-request" code-review-forge-pr-at-point)))
+;;  (with-eval-after-load 'forge
+;;    (transient-append-suffix 'forge-dispatch "c u"
+;;      '("c r" "review pull-request" code-review-forge-pr-at-point))))
+
+(provide 'init-git)
