e81a306da4da44dd675d7f144e891e6b2521d505
Mathias Rüdiger <ruediger@blueboot.org>
2024-02-13 15:45:26 +0100

    setup diff-hl

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -25,4 +25,15 @@
 ;;    (transient-append-suffix 'forge-dispatch "c u"
 ;;      '("c r" "review pull-request" code-review-forge-pr-at-point))))
 
+(use-package diff-hl
+  :straight t
+  :hook (find-file . diff-hl-mode)
+  :hook (dired-mode . diff-hl-dired-mode)
+  :hook (vc-dir-mode . diff-hl-dir-mode)
+  :hook (diff-hl-mode . diff-hl-flydiff-mode)
+  :hook (magit-pre-refresh . diff-hl-magit-pre-refresh)
+  :hook (magit-post-refresh . diff-hl-magit-post-refresh)
+  :custom
+  (diff-hl-draw-borders nil))
+
 (provide 'init-git)
