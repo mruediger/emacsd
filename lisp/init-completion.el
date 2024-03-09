2d345cd0da42192565ee335d71e8d11ba5ff7a7e
Mathias Rüdiger <ruediger@blueboot.org>
2024-03-09 16:41:30 +0100

    switch from corfu to company

--- lisp/init-completion.el
+++ lisp/init-completion.el
@@ -2,28 +2,13 @@
   :init
   (use-package yasnippet-snippets :straight t))
 
-(use-package corfu :straight t
-  ;; Optional customizations
-  ;; :custom
-  ;; (corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
-  ;; (corfu-auto t)                 ;; Enable auto completion
-  ;; (corfu-separator ?\s)          ;; Orderless field separator
-  ;; (corfu-quit-at-boundary nil)   ;; Never quit at completion boundary
-  ;; (corfu-quit-no-match nil)      ;; Never quit, even if there is no match
-  ;; (corfu-preview-current nil)    ;; Disable current candidate preview
-  ;; (corfu-preselect 'prompt)      ;; Preselect the prompt
-  ;; (corfu-on-exact-match nil)     ;; Configure handling of exact matches
-  ;; (corfu-scroll-margin 5)        ;; Use scroll margin
-  :init
-  (global-corfu-mode))
+(use-package company :straight t
+  :hook (eglot-managed-mode . company-mode))
 
 (use-package orderless
   :straight t
   :init
   (setq completion-styles '(substring orderless basic)))
-
-;;completion-category-defaults nil
-;;completion-category-overrides '((file (styles partial-completion)))))
 
 ;; minimalistic vertical completion UI
 (use-package vertico
