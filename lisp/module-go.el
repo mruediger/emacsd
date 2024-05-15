d67b2b2e6edc4fb9ca817c4c7afcfd72ca12dc10
Mathias Rüdiger <ruediger@blueboot.org>
2024-05-15 10:07:40 +0200

    use compile + go test instead of gotest

--- lisp/module-go.el
+++ lisp/module-go.el
@@ -1,19 +1,16 @@
 (require 'eglot)
 
-(straight-use-package 'gotest)
-
 (add-to-list 'auto-mode-alist '("\\.go\\'" . go-ts-mode))
 
 (with-eval-after-load 'go-ts-mode
-  (keymap-set go-ts-mode-map "C-c C-c" 'go-test-current-project)
-
   ;; CamelCase aware editing operations
   (subword-mode +1)
 
   (setq go-ts-mode-indent-offset 2)
   (setq go-test-verbose t)
 
-  (add-hook 'go-ts-mode-hook (lambda ()
-                               (setq tab-width 2))))
+  (keymap-set go-ts-mode-map "C-c C-c" 'compile)
+  (add-hook 'go-ts-mode-hook (lambda () (setq-local compile-command (concat "go test"))))
+  (add-hook 'go-ts-mode-hook (lambda () (setq tab-width 2))))
 
 (provide 'module-go)
