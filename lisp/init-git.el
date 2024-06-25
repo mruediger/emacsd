10c9a2ba15602be37f7f2fae5b4463a21d0695c8
Mathias Rüdiger <ruediger@blueboot.org>
2024-06-25 15:05:10 +0200

    remove code-review

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -10,33 +10,6 @@
 (use-package forge :straight t
   :after magit)
 
-(use-package code-review
-  :straight (:host github :repo "phelrine/code-review" :branch "fix/closql-update")
-  :after magit
-  :custom
-  (code-review-download-dir (concat my-cache-dir "code-review/"))
-  (code-review-db-database-file (concat my-local-dir "code-review/database.sqlite"))
-  (code-review-log-file (concat my-local-dir "code-review/code-review-error.log"))
- ; use the same credentials as forge in ~/.authinfo.gpg
-  (code-review-auth-login-marker 'forge))
-
-
-;;(use-package code-review
-;;  :straight (:host github :repo "phelrine/code-review" :branch "fix/closql-update")
-;;  :after magit
-;;  :custom
-;;  (code-review-download-dir (concat minemacs-cache-dir "code-review/"))
-;;  (code-review-db-database-file (concat minemacs-local-dir "code-review/database.sqlite"))
-;;  (code-review-log-file (concat minemacs-local-dir "code-review/code-review-error.log"))
-;;  (code-review-auth-login-marker 'forge) ; use the same credentials as forge in ~/.authinfo.gpg
-;;  :init
-;;  (with-eval-after-load 'magit
-;;    (transient-append-suffix 'magit-merge "i"
-;;      '("y" "Review pull-request" code-review-forge-pr-at-point)))
-;;  (with-eval-after-load 'forge
-;;    (transient-append-suffix 'forge-dispatch "c u"
-;;      '("c r" "review pull-request" code-review-forge-pr-at-point))))
-
 (use-package git-link
   :straight t)
 
