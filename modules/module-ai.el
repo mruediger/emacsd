1fa70e1537ac1e499c0fc75290b577f7fa4492a8
Mathias Rüdiger <ruediger@blueboot.org>
2024-11-29 10:10:00 +0100

    setup gptel

--- modules/module-ai.el
+++ modules/module-ai.el
@@ -5,4 +5,21 @@
 
 (use-package dall-e-shell :straight t)
 
+
+(use-package gptel :straight t
+  :config
+  (setq gptel-default-mode 'org-mode
+        gptel-api-key (auth-source-pass-get 'secret "justwatch/openai-key"))
+
+  (gptel-make-gemini "Gemini"
+    :key (auth-source-pass-get 'secret "cloud/gemini-n96")
+    :stream t)
+
+  :bind (("C-c RET" . gptel-send)))
+
+;;(use-package magit-magit-gptcommit :straight t
+;;  :after magit
+;;  :bind (:map git-commit-mode-map
+;;              ("C-c C-g" . magit-gptcommit-commit-accept))
+
 (provide 'module-ai)
