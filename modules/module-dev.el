3bced7f181026451bcd7523fcbf73da88c552d2a
Mathias Rüdiger <ruediger@blueboot.org>
2025-02-19 14:11:36 +0100

    move compile related use-packages to module-dev

--- modules/module-dev.el
+++ modules/module-dev.el
@@ -18,4 +18,31 @@
   :custom
   (whitespace-style '(face trailing)))
 
+(use-package python
+  :init
+  (setq-default indent-tabs-mode nil)
+  :config
+  (setq python-indent-offset 4)
+  :hook
+  (python-mode . (lambda ()
+                   (setq-local compile-command (concat "python " buffer-file-name))))
+  :bind (:map python-mode-map
+              ("C-c C-c" . compile)))
+
+(use-package rust-ts-mode
+  :mode "\\.rs\\'"
+  :hook
+  (rust-ts-mode . (lambda ()
+                   (setq-local compile-command "cargo test -- --nocapture")))
+  :bind (:map rust-ts-mode-map
+              ("C-c C-c" . compile)))
+
+(use-package nix-mode :straight t
+  :config
+  (defun nix-update () (interactive)
+         (let ((default-directory "/sudo::"))
+           (compile "nixos-rebuild switch --flake '/home/bag/src/nixos/src#'")))
+  :bind (:map nix-mode-map ("C-c C-c" . nix-update)))
+
+
 (provide 'module-dev)
