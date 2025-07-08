f885ac7d9a28f0cce1ffed29a11181c487ecd66e
Mathias Rüdiger <ruediger@blueboot.org>
2025-07-08 09:54:18 +0200

    setup polymode for helm templates

--- modules/module-yaml.el
+++ modules/module-yaml.el
@@ -6,4 +6,15 @@
   (setq highlight-indent-guides-method 'character)
   :hook (yaml-ts-mode . highlight-indent-guides-mode))
 
+(define-hostmode poly-yaml-ts-hostmode :mode 'yaml-ts-mode)
+(define-innermode poly-go-ts-template-innermode :mode 'go-ts-mode
+  :head-matcher "{{"
+  :tail-matcher "}}"
+  :head-mode 'body
+  :tail-mode 'body)
+
+(define-polymode helm-template-mode
+  :hostmode 'poly-yaml-ts-hostmode
+  :innermodes '(poly-go-ts-template-innermode))
+
 (provide 'module-yaml)
