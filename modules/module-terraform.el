d3e2b4400aa8e00c49eb25ab1cedc7d829aa8210
Mathias Rüdiger <ruediger@blueboot.org>
2025-07-08 09:55:38 +0200

    add terraform compile command

--- modules/module-terraform.el
+++ modules/module-terraform.el
@@ -1,6 +1,7 @@
 (use-package terraform-mode
   :mode ("\\.tf" . terraform-mode)
   :hook
+  (terraform-mode . (lambda () (setq-local compile-command "terraform plan -no-color")))
   ;; workarround for https://github.com/hashicorp/terraform-ls/issues/1067
   (terraform-mode . (lambda () (setq-local create-lockfiles nil)))
   (terraform-mode . eglot-ensure)
