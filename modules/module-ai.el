43b851ea6889a1200853fcc9b7c398cbe0eae9c7
Mathias Rüdiger <ruediger@blueboot.org>
2025-10-08 14:45:58 +0200

    try some mcp stuff

--- modules/module-ai.el
+++ modules/module-ai.el
@@ -14,7 +14,35 @@
   (setq gptel-backend gptel-backend-claude
         gptel-model 'claude-3-5-sonnet-20241022)
 
+  (gptel-make-tool
+   :name "create_file"
+   :function (lambda (path filename content)   ; the function that runs
+               (let ((full-path (expand-file-name filename path)))
+                 (with-temp-buffer
+                   (insert content)
+                   (write-file full-path))
+                 (format "Created file %s in %s" filename path)))
+   :description "Create a new file with the specified content"
+   :args (list '(:name "path"             ; a list of argument specifications
+	               :type string
+               '(:name "filename"
+	               :type string
+	               :description "The name of the file to create")
+               '(:name "content"
+	               :type string
+	               :description "The content to write to the file"))
+   :category "filesystem"))
   :bind (("C-c C-<return>" . gptel-send)))
+
+(use-package mcp
+  :after gptel
+  :config (require 'mcp-hub)
+  :custom
+  (mcp-hub-servers
+   `(("fetch" . (:command "uvx" :args ("mcp-server-fetch"))))))
+
+(use-package gptel-integrations
+   :after (gptel mcp))
 
 (use-package aider
   :vc (:url "git@github.com:tninja/aider.el.git")
