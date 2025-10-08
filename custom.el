d1d07b6f8c0fb7f2b0ce8af36a8c834dd65db8db
Mathias Rüdiger <ruediger@blueboot.org>
2025-10-08 14:46:33 +0200

    add claude-code stuff

--- custom.el
+++ custom.el
@@ -5,6 +5,12 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(auth-source-save-behavior nil)
+ '(package-selected-packages '(claude-code jira mcp monet))
+ '(package-vc-selected-packages
+   '((claude-code :url "https://github.com/stevemolitor/claude-code.el")
+     (monet :url "https://github.com/stevemolitor/monet")
+     (jira :url "git@github.com:unmonoqueteclea/jira.el.git")
+     (mcp :url "https://github.com/lizqwerscott/mcp.el.git")))
  '(safe-local-variable-values '((use-sudo-compile . t))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
