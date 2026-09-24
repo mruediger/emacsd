8cf40aa18703a9605d46a210f594c5f3078a7d8c
Mathias Rüdiger <ruediger@blueboot.org>
2026-09-24 10:46:42 +0200

    update smtpmail settings

--- modules/init-email.el
+++ modules/init-email.el
@@ -2,9 +2,9 @@
 (setq user-mail-address "ruediger@blueboot.org")
 
 (setq send-mail-function 'smtpmail-send-it
-      smtpmail-stream-type 'starttls
-      smtpmail-smtp-server "smtp.gmail.com"
-      smtpmail-smtp-service 587
+      smtpmail-stream-type 'ssl
+      smtpmail-smtp-server "smtp.mailbox.org"
+      smtpmail-smtp-service 465
       smtpmail-debug-verb t)
 
 (add-hook 'mail-mode-hook 'visual-line-mode)
